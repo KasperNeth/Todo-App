185ea3e6008fbc1fdfdf8c47bd54b097ff225614
KasperNeth <okeowoa244@gmail.com>
2024-12-12 02:19:45 +0100

    Add: Auth functionality

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,31 @@
+const TaskService = require('../services/task.service')
+
+const CreateTask = async (req, res) => {
+
+}
+
+const GetAllTasks = async (req, res) => {
+
+}
+
+const GetTask = async (req, res) => {
+
+}
+
+const UpdateTask = async (req, res) => {
+
+}
+
+const DeleteTask = async (req, res) => {
+
+}
+
+
+
+module.exports = {
+  CreateTask,
+  GetAllTasks,
+  GetTask,
+  UpdateTask,
+  DeleteTask
+}
