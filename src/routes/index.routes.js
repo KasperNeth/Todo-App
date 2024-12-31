c089697ee082d00a8cc80e78554002e1d09b3e3d
KasperNeth <okeowoa244@gmail.com>
2024-12-31 17:52:24 +0100

    add: views

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,13 +1,12 @@
 const Router = require("express").Router;
 const Authroutes = require("./auth.routes")
 const Taskroutes = require("./task.routes")
+const tasksdashboard = require("./dashboard.routes")
 
 const route = Router()
 
 route.use("/auth", Authroutes)
-route.use("/task", Taskroutes)
-// route.use("/task", Taskroutes)
-
-
+route.use("/tasks", Taskroutes)
+route.use("/dashboard", tasksdashboard)
 
 module.exports = route;
